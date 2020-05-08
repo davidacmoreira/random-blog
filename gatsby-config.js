92c98529dde1df60733b299bcf0471de25aa448e
David Moreira <davidmoreira37@gmail.com>
2020-05-08 08:23:30 +0100

    Add configuration Gatsby Node API

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,4 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "random"
   },
+  plugins : [
+    "gatsby-plugin-catch-links",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/content/posts`
+      }
+    }
+  ]
 }
