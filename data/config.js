607864ca6fd71f3f503106ce4e6e6a34dc68bb4a
David Moreira <davidmoreira37@gmail.com>
2020-05-26 09:23:51 +0100

    Fix site config

--- data/config.js
+++ data/config.js
@@ -2,12 +2,12 @@ const config = {
     siteTitle: 'random',
     siteDescription: 'random',
     siteTitleShort: 'random',
-    siteRss: "/rss.xml",
-    siteImage: "",
-    googleAnalyticsID: "random-blog",
-    themeColor: "#000000",
-    backgroundColor: "#F2F3F4",
-    siteUrl: "https://random-blog.netlify.app",
+    siteRss: '/rss.xml',
+    siteImage: '',
+    googleAnalyticsID: 'random-blog',
+    themeColor: '#000000',
+    backgroundColor: '#F2F3F4',
+    siteUrl: 'https://random-blog.netlify.app'
 };
 
 module.exports = config;
